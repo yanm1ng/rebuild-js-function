f2fafa243d221eb759402cdf08579817e364dd10
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-03-07 17:29:17 +0800

    added unshift.js more ways

--- unshift.js
+++ unshift.js
@@ -10,6 +10,16 @@ Array.prototype.newUnshift = function(item) {
   return res;
 }
 
+Array.prototype.newUnshift = function(item) {
+  var arr = [item].concat(this);
+  return arr;
+}
+
+Array.prototype.newUnshift = function(item) {
+  this.splice(0, 0, item);
+  return this;
+}
+
 var a = [1, 2, 3];
 var b = a.newUnshift(0);
 console.log(b);
