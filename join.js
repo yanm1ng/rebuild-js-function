0fac15b7578d7751e5d6e3ac231108fcc9daa3c8
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-03-07 21:26:16 +0800

    added join.js

---
+++ join.js
@@ -0,0 +1,27 @@
+Array.prototype.newJoin = function(str) {
+  var res = '';
+  for (let i=0; i < this.length; i++) {
+    if (i === this.length - 1) {
+      res += this[i];
+    } else {
+      res += this[i] + str;
+    }
+  }
+  return res;
+}
+
+Array.prototype.newJoin = function(str) {
+  var times = this.length - 1;
+  var res = '';
+
+  for (let i=0; i < times; i++) {
+    this.splice(i * 2 + 1, 0, str);
+  }
+  for (let i=0; i < this.length; i++) {
+    res += this[i];
+  }
+  return res;
+}
+
+var arr = [1, 2, 3];
+console.log(arr.newJoin('x'));
