222939e2ba74394fe184aa5c8dcf2e68273fab55
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-03-19 20:13:41 +0800

    added removeClassName.js function

---
+++ removeClassName.js
@@ -0,0 +1,12 @@
+function removeClassName (node, className) {
+  var classNames = node.className.split(/\s+/) | [];
+  var pos = -1;
+  for (let i = 0; i < classNames.length; i++) {
+    if (classNames[i] === className) {
+      pos = i;
+      break;
+    }
+  }
+  classNames.splice(pos, 1);
+  node.className = classNames.join(' ');
+}
