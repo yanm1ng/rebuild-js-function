52709fa749bd1980e8f15c2097cf2101c89b5aeb
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-03-20 20:17:58 +0800

    added fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+  var fibarr = [];
+  var i = 0;
+  while (i < n) {
+    if (i <= 1) {
+      fibarr.push(i);
+    } else {
+      fibarr.push(fibarr[i - 1] + fibarr[i - 2])
+    }
+    i++;
+  }
+  return fibarr;
+}
+
+console.log(fibonacci(5));
